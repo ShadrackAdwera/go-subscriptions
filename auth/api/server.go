203f0e2e80d8a994321d55d1325d1f534c91a367
ShadrackAdwera <adweshshaddie@gmail.com>
2023-04-20 09:11:08 +0300

    add error handler / api routes

--- auth/api/server.go
+++ auth/api/server.go
@@ -19,10 +19,16 @@ func NewServer(store db.TxStore, maker token.TokenMaker) *Server {
 		store:      store,
 		tokenMaker: maker,
 	}
-
 	// add routes
+	router.POST("/auth/sign-up", srv.signUp)
+	router.POST("/auth/login", srv.login)
+
 	srv.router = router
 	return srv
+}
+
+func errJSON(err error) gin.H {
+	return gin.H{"message": err.Error()}
 }
 
 func (s *Server) StartServer(addr string) error {
