e835f16d531ee80606790683c601d1a6a1f808da
ShadrackAdwera <adweshshaddie@gmail.com>
2023-04-20 09:10:51 +0300

    set up test main for handlers

---
+++ auth/api/main_test.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	db "github.com/ShadrackAdwera/go-subscriptions/db/sqlc"
+	"github.com/ShadrackAdwera/go-subscriptions/token"
+	"github.com/ShadrackAdwera/go-subscriptions/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func newServer(store db.TxStore) *Server {
+	tknMaker, err := token.NewPasetoMaker(utils.RandomString(32))
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	srv := NewServer(store, tknMaker)
+	return srv
+}
+
+func TestMain(m *testing.M) {
+	gin.SetMode(gin.TestMode)
+	os.Exit(m.Run())
+}
