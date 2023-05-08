a7b3038c521500903ceaaabf490786f103f7e5cd
ShadrackAdwera <adweshshaddie@gmail.com>
2023-05-08 07:00:17 +0300

    add stripe id to user schema

--- subscriptions/db/query/users.sql
+++ subscriptions/db/query/users.sql
@@ -1,19 +1,26 @@
 -- name: CreateSubscriptionUser :one
 INSERT INTO users (
-  id, username, email
+  id, username, email, stripe_id
 ) VALUES (
-  $1, $2, $3
+  $1, $2, $3, $4
 )
 RETURNING *;
 
 -- name: GetSubscriptionUsers :many
 SELECT * FROM users;
 
+-- name: GetSubscriptionUserByStripeId :one
+SELECT * 
+FROM users 
+WHERE stripe_id = $1 
+LIMIT 1;
+
 -- name: UpdateSubscriptionUser :one
 UPDATE users 
 SET
   username = COALESCE(sqlc.narg(username),username),
-  email = COALESCE(sqlc.narg(email),email)
+  email = COALESCE(sqlc.narg(email),email),
+  stripe_id = COALESCE(sqlc.narg(stripe_id),stripe_id)
 WHERE id = sqlc.arg(id)
 RETURNING *;
 
